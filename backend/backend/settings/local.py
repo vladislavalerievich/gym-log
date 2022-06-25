6a0f6106c70cbd5d43547243fd48ba4f2e06a376
vmoroshan <vlad.moroshan@gmail.com>
2022-06-25 13:23:42 +0300

    Add local.py

---
+++ backend/backend/settings/local.py
@@ -0,0 +1,10 @@
+from backend.settings.base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
