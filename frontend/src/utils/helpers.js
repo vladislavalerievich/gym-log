2c16e49a2236275c47df688d0347563758561807
vmoroshan <vlad.moroshan@gmail.com>
2022-06-25 14:26:34 +0300

    Remove isTokenValid function

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,12 +1,3 @@
-import jwt_decode from 'jwt-decode';
-
-
-export const isTokenValid = (token) => {
-    const decodedToken = jwt_decode(token);
-    const currentDate = new Date();
-    return decodedToken.exp * 1000 > currentDate.getTime();
-}
-
 export const timestampToString = (timestamp) => {
     const date = new Date(timestamp);
     return date.toDateString().toLocaleString();
