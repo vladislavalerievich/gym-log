3edaa7c784ce6b52621888c0124a4bcf59b2595e
vmoroshan <vlad.moroshan@gmail.com>
2022-06-28 17:21:58 +0300

    Remove directory scripts

--- backend/scripts/entrypoint-prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-python manage.py loaddata backend/core/fixtures/exercise.json
-
-python backend/manage.py runserver 0.0.0.0:$PORT
