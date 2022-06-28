ada7d8f2cd4e2922604a6628dd5bbc16801765a8
vmoroshan <vlad.moroshan@gmail.com>
2022-06-28 18:55:18 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {useQuery} from "react-query";
 import {ToastContainer} from "react-toastify";
@@ -21,7 +21,7 @@ import * as profileApi from "./api/profileApi";
 
 const App = () => {
     const dispatch = useDispatch();
-    const authenticated = useSelector((state) => state.auth.authenticated)
+    const authenticated = useSelector((state) => state.auth.authenticated);
     // Fetch initial data that will be used by several components
     useQuery('bodyParts', exerciseApi.getBodyParts, {staleTime: Infinity, enabled: authenticated});
     useQuery('equipment', exerciseApi.getEquipment, {staleTime: Infinity, enabled: authenticated});
@@ -42,7 +42,7 @@ const App = () => {
                         <Route path={pageRoutes.workout} element={<ProtectedRoute><Workout/></ProtectedRoute>}/>
                         <Route path={pageRoutes.history} element={<ProtectedRoute><WorkoutHistory/></ProtectedRoute>}/>
                         <Route path={pageRoutes.exercises} element={<ProtectedRoute><Exercises/></ProtectedRoute>}/>
-                        <Route path="*" element={<ProtectedRoute><NotFound/></ProtectedRoute>}/>
+                        <Route path="*" element={authenticated ? <Navigate to={pageRoutes.workout}/> : <NotFound/>}/>
                     </Routes>
                 </Container>
             </Router>
