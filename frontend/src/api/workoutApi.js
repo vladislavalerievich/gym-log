f4ae5a53babcab0b15cebebe62ddaa6205c5ca0c
vmoroshan <vlad.moroshan@gmail.com>
2022-06-28 17:11:34 +0300

    Update workoutApi.js

--- frontend/src/api/workoutApi.js
+++ frontend/src/api/workoutApi.js
@@ -3,4 +3,4 @@ import {apiRoutes} from "../utils/routes";
 
 export const getWorkoutHistory = () => axiosInstance.get(apiRoutes.workout).then(response => response.data);
 export const createWorkout = (data) => axiosInstance.post(apiRoutes.workout, data).then(response => response.data);
-export const deleteWorkout = (id) => axiosInstance.delete(apiRoutes.workout + id).then(response => response.data);
+export const deleteWorkout = (id) => axiosInstance.delete(apiRoutes.workout + id + '/').then(response => response.data);
