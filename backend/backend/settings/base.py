6da8a4682c1156cba5b19bd0b6ad23b78cdfead4
vmoroshan <vlad.moroshan@gmail.com>
2022-06-28 19:04:49 +0300

    Update settings/base.py

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
