04e52a1ff8c43851fbce4cf8c797677a3fda34f4
vmoroshan <vlad.moroshan@gmail.com>
2022-06-28 17:17:27 +0300

    Update prod.py

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -13,12 +13,11 @@ MIDDLEWARE += [
 ]
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 
 TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "frontend", "build")]
 WHITENOISE_ROOT = os.path.join(BASE_DIR, "frontend", "build", "root")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend", "build", "static")]
