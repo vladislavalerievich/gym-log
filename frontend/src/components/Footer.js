0d7902668008bbb7bed1dbd196d6cdabc421cdc9
vmoroshan <vlad.moroshan@gmail.com>
2022-06-29 13:50:54 +0300

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,14 +2,16 @@ const Footer = () => {
     return (
         <footer className="text-center bg-light pt-1">
             <span>Made with <i className="fa fa-heart"/> by </span>
-            <a
-                href={process.env.REACT_APP_AUTHOR_LINKEDIN_URL}
-                target="_blank"
-                rel="noreferrer"
-            >
+            <a href={process.env.REACT_APP_AUTHOR_LINKEDIN_URL} target="_blank" rel="noreferrer">
                 Vladislav Moroshan
             </a>
-            <div> ©2022 {process.env.REACT_APP_NAME}</div>
+
+            <div>
+                <a href={process.env.REACT_APP_GITHUB_URL} target="_blank" rel="noreferrer">
+                    <i className="fab fa-github"/> {process.env.REACT_APP_NAME}
+                </a>
+                <span> ©2022</span>
+            </div>
         </footer>
     );
 };
